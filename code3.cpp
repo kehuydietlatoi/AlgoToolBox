f6a099308869271faeda1d9ab0dfee04252c8630
kehuydietlatoi <donghungcp98@gmail.com>
2021-03-28 04:16:01 +0200

    make code cleaner

--- code3.cpp
+++ code3.cpp
@@ -7,7 +7,6 @@
 using namespace std;
 void solve() {
     int N,C;
-    int res = 0;
     std::vector<int> v;
     cin >> N >> C;
     if (C < N-1) {
@@ -18,34 +17,10 @@ void solve() {
         v.push_back(i);
     }
     C = C - N + 1;
-    int MAX = N;
-    for (auto i = 0; i < N-1; i++) {
-        if(C >= N-1 -i){ //too big
-            std::vector<int>::iterator itBig = find (v.begin(), v.end(), MAX);
-            std::vector<int>::iterator itSmall = find (v.begin(), v.end(), i+1);
-            for (auto it = v.begin(); it != v.end(); ++it) {
-                if (*it == *itSmall) {
-                    std::reverse(itSmall,itBig+1);break;
-                }
-                if (*it == *itBig) {
-                    std::reverse(itBig,itSmall+1);break;
-                }
-            }
-            C = C - (N-1-i);
-        }
-        else if (C < N -1-i){
-            std::vector<int>::iterator itBig = find (v.begin(), v.end(), MAX);
-            std::vector<int>::iterator itSmall = find (v.begin(), v.end(), MAX-C);
-            for (auto it = v.begin(); it != v.end(); ++it) {
-                if (*it == *itSmall) {
-                    std::reverse(itSmall,itBig+1);break;
-                }
-                if (*it == *itBig) {
-                    std::reverse(itBig,itSmall+1);break;
-                }
-            }
-            C = 0;break;
-        }
+    for (auto i = N-1; i >=0; i--) {
+        int minCost = min(C,N-1-i);
+        C -= minCost;
+        reverse(v.begin()+i,v.begin()+i+minCost+1);
     }
     if(C != 0){
         std::cout << " IMPOSSIBLE" << '\n'; return;
